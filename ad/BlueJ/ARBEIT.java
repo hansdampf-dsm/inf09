8c7f3d9abb116b5bbed657e3792cc16d81b473e6
Alberto Duran <alberto.duran@student.dsmadrid.org>
2021-03-24 13:59:05 +0100

    furbo

---
+++ ad/BlueJ/ARBEIT.java
@@ -0,0 +1,31 @@
+
+//Dies ist ein Kommentar, das liest nur der Programmierer 
+class ARBEIT
+{
+//Der Inhalt einer Klasse wird durch zwei geschweifte Klammern zusammengehalten
+    
+//Hier werden drei Objekte deklariert
+    WELT welt;
+    ROBOTER karola;
+    
+    
+    
+    public ARBEIT()
+    {
+        //Hier werden die drei Objekte erzeugt
+        welt = new WELT( 20,20,20);
+        karola = new ROBOTER(welt);
+        
+        
+        
+    }
+    
+    void beispielMethode()
+    {
+        //diese Methode tut noch nix
+        
+    }
+        
+  
+//dies ist die Klammer, die die Klasse schlieﬂt
+}
